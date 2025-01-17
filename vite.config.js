a33c05bd86082b1ff857a2b65a671637c6bae3d8
yosket <yosket87@gmail.com>
2025-01-17 22:06:25 +0900

    feat: set various configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import svgLoader  from 'vite-loader-svg'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    open: true,
+  },
+  static:{
+    directory:'public',
+    serveDirectory: true
+  },
+  plugins: [react(), svgLoader()],
 })
+
